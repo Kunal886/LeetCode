7700cba0ecd1db9a0a53b122b01d6a89680dc309
Kunal886 <singhkunal0481@gmail.com>
2026-09-05 15:04:23 +0530

    Time: 23 ms (31.57%) | Memory: 92.9 MB (79.40%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int i:nums){
+            set.add(i);
+        }
+        return set.size()!=nums.length;
+
+    }
+}
