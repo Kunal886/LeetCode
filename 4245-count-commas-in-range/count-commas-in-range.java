d32fc831bf66b2a9f985c4c43b09f13f1fb042cb
Kunal886 <singhkunal0481@gmail.com>
2026-09-08 21:32:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (57.08%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -3,12 +3,6 @@ class Solution {
         if(n>=1000 && n<=100000)
         return n-999;
 
-        //else if(n>10000 && n<100000)
-      //  return n-9999+9000;
-
-        //else if(n==100000)
-       // return n-999;
-
         return 0;
     }
 }
