ac0f4daa9e63919565b1738d82e68f4e018a1f45
Kunal886 <singhkunal0481@gmail.com>
2026-09-16 12:41:16 +0530

    Time: 5 ms (30.86%) | Memory: 46.9 MB (19.90%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,58 +1,26 @@
 class Solution {
     public int romanToInt(String s) {
-      int num=0;
-       for(int i=1;i<s.length();i++){
-          
-          if(s.charAt(i)=='I'){
-            num++;
-          }
-          else if(s.charAt(i)=='V'){
-            if(s.charAt(i-1)=='I'){
-                num+=3;
-            }
-            else num+=5;
-          }
-          else if(s.charAt(i)=='X'){
-            if(s.charAt(i-1)=='I'){
-                num+=8;
-            }
-            else num+=10;
-          }
-          else if(s.charAt(i)=='L'){
-            if(s.charAt(i-1)=='X'){
-                num+=30;
-            }
-            else num+=50;
-          }
-           else if(s.charAt(i)=='C'){
-            if(s.charAt(i-1)=='X'){
-                num+=80;
-            }
-            else num+=100;
-          }
-           else if(s.charAt(i)=='D'){
-            if(s.charAt(i-1)=='C'){
-                num+=300;
-            }
-            else num+=500;
-          }
-            else if(s.charAt(i)=='M'){
-            if(s.charAt(i-1)=='C'){
-                num+=800;
-            }
-            else num+=1000;
-          }
-       }
-       char ch=s.charAt(0);
-       if(ch=='I') num+=1;
-       else if(ch=='V') num+=5;
-       else if(ch=='X') num+=10;
-         else if(ch=='L') num+=50;
-           else if(ch=='C') num+=100;
-             else if(ch=='D') num+=500;
-              else num+=1000;
-       
+        HashMap<Character,Integer> map=new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
 
-       return num;
+        int sum=0;
+        for(int i=0;i<s.length()-1;i++){
+           if(map.get(s.charAt(i))>=map.get(s.charAt(i+1))){
+            sum+=map.get(s.charAt(i));
+           }
+           else 
+           sum-=map.get(s.charAt(i));
+            
+            
+
+        }
+        sum+=map.get(s.charAt(s.length()-1));
+        return sum;
     }
 }
